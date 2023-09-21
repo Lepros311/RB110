52e8f81775416f67e66e70d98d0fd9960a7d6628
Andrew S <andy.strickland@gmail.com>
2023-09-21 19:24:02 -0400

    Add Medium_1 folder to Small_Problems

---
+++ Small_Problems/Medium_1/1_Rotation_1.rb
@@ -0,0 +1,41 @@
+# Problem:
+# Write a method that rotates an array by moving the first element to the end of the array. The original array should not be modified.
+
+# Do not use the method Array#rotate or Array#rotate! for your implementation.
+
+# Test cases:
+# rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+# rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+# rotate_array(['a']) == ['a']
+
+# x = [1, 2, 3, 4]
+# rotate_array(x) == [2, 3, 4, 1]   # => true
+# x == [1, 2, 3, 4]                 # => true
+
+# input:
+# array
+
+# output:
+# a new array with the first element moved to be the last element
+
+# rules:
+  # 
+
+  #Implicit requirements:
+  # an array of a single element will return another array that looks the same 
+
+# Algorithm:
+# 
+
+def rotate_array(arr)
+  new_arr = []
+  arr.each_with_index do |el, i|
+    next if i == 0
+    new_arr << el
+  end
+  new_arr << arr[0]
+end
+
+puts rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+puts rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+puts rotate_array(['a']) == ['a']
